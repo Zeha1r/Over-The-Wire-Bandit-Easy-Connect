7b2b0a0534fb32d2fc1d830e06f20f9943b6a89c
FuzzyVee <nfenixer@gmail.com>
2026-02-23 09:20:46 +0100

    Fix sshpass command and update password extraction

--- makeit.sh
+++ makeit.sh
@@ -4,7 +4,7 @@ mkdir -p $HOME/etc/otw/ && touch $HOME/etc/otw/passwds.txt
 echo "
 sso() {
         if [ $# -eq 2 ]; then
-                shpass -p "$2" ssh "bandit$1@bandit.labs.overthewire.org" -p 2220
+                sshpass -p "$2" ssh "bandit$1@bandit.labs.overthewire.org" -p 2220
                 STATUS=$?
                 if [ $STATUS -eq 0]; then
                         sed -i "$1s/.*/bandit$1:$2/" $HOME/etc/otw/passwds.txt
@@ -29,7 +29,7 @@ sso() {
 
         elif [ $# -eq 0 ]; then
                 last=$(grep -E "bandit[0-9]*:" $HOME/etc/otw/passwds.txt | tail -n 1)
-                num1=$(echo "$last" | sed -E 's/.*bandit([0-9]+).*/\1/')
+                num1=$(echo "$last" | sed -E 's/.*bandit([0-9]+).*/\1/' | tr -d "a-zA-Z" )
                 num2=$(echo "$last" | cut -d':' -f2-)
                 sshpass -p "$num2" ssh "bandit$num1@bandit.labs.overthewire.org" -p 2220
         fi
